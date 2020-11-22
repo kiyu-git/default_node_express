265166c7706432cf7445d9ad384066129da9bf9f
Nishida Kiyu <kiyu@raifu-MAC-36noMacBook-puro-2.local>
2020-11-22 17:12:30 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// express モジュールのインスタンス作成
+const express = require('express');
+const app = express();
+// パス指定用モジュール
+const path = require('path');
+const port = 3000;
+
+// 8080番ポートで待ちうける
+app.listen(port, () => {
+  console.log('Running at Port', port);
+});
+
+// 静的ファイルのルーティング
+app.use(express.static(path.join(__dirname, 'public')));
+
+// その他のリクエストに対する404エラー
+app.use((req, res) => {
+  res.sendStatus(404);
+});
